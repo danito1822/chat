141609c5333bfac936d796effd80816a74c27ff8
danito1822 <117946614+danito1822@users.noreply.github.com>
2025-02-12 11:48:35 -0400

    Update chat.go

--- chat.go
+++ chat.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var addr = flag.String("addr", ":8090", "http service address")
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
